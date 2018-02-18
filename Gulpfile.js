e29dcd6fb6f7eb7982f46f95b3b9540443bf9cf0
Pavel Mineev <pavel@mineev.me>
2018-02-18 15:39:15 +0300

    short class names, better html minify

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,46 +2,80 @@ const gulp = require('gulp')
 const htmlmin = require('gulp-htmlmin')
 const inline = require('gulp-inline')
 const csso = require('gulp-csso')
+const rimraf = require('gulp-rimraf')
 const uglify = require('gulp-uglify-es').default
 const connect = require('gulp-connect')
+const gulpMinifyCssNames = require('gulp-minify-cssnames')
 const ghPages = require('gulp-gh-pages')
 
+const SRC_DIR = './public'
+const DEST_DIR = './dist'
+const SRC_CWD = { cwd: SRC_DIR }
+const DEST_CWD = { cwd: DEST_DIR }
+const UGLIFY_OPTS = { mangle: false }
+const HTMLMINIFY_OPTS = {
+	collapseWhitespace: true,
+	removeComments: true,
+	sortAttributes: true,
+}
 
-gulp.task('copy', () =>
-	gulp.src('./public/*.!(js|css)')
-		.pipe(connect.reload())
-		.pipe(gulp.dest('./dist'))
+
+// minify class names and copy to dest dir
+gulp.task('minify-classnames', () =>
+	gulp.src('*.{css,html,js}', SRC_CWD)
+		.pipe(gulpMinifyCssNames({
+			prefix: 'pm--',
+			postfix: '',
+		}))
+		.pipe(gulp.dest(DEST_DIR))
 )
 
-gulp.task('html', () =>
-	gulp.src('./public/index.html')
+// copy unprocessible files to dest dir
+gulp.task('copy-other-files', () =>
+	gulp.src(['*.!(html|js|css)', 'CNAME'], SRC_CWD)
+		.pipe(connect.reload())
+		.pipe(gulp.dest(DEST_DIR))
+)
+
+// minify and insert css and js to html
+gulp.task('minify-html-inline', () =>
+	gulp.src('index.html', DEST_CWD)
 		.pipe(inline({
-			base: './public',
+			base: DEST_DIR,
 			css: csso,
-			js: () => uglify({ mangle: false }),
+			js: () => uglify(UGLIFY_OPTS),
 			disabledTypes: ['svg', 'img'],
 		}))
-		.pipe(htmlmin({ collapseWhitespace: true }))
+		.pipe(htmlmin(HTMLMINIFY_OPTS))
 		.pipe(connect.reload())
-		.pipe(gulp.dest('./dist'))
+		.pipe(gulp.dest(DEST_DIR))
+)
+
+// remove inlined css and js from dest dir
+gulp.task('cleanup-build', () =>
+	gulp.src('*.{js,css}', DEST_CWD)
+		.pipe(rimraf())
 )
 
 gulp.task('serve', () => {
-	gulp.watch('./public/*.(html|css|js)', gulp.series('html'))
-	gulp.watch('./public/*.!(html|css|js)', gulp.series('copy'))
+	gulp.watch('./public/*.(html|css|js)', gulp.series('minify-classnames', 'minify-html-inline', 'cleanup-build'))
+	gulp.watch('./public/*.!(html|css|js)', gulp.series('copy-other-files'))
 
 	connect.server({
-		root: [ './dist', './public' ],
+		root: [ DEST_DIR, SRC_DIR ],
 		livereload: true,
 		debug: true,
 	})
 })
 
 gulp.task('gh-pages', () =>
-	gulp.src('./dist/**/*')
+	gulp.src(`${DEST_DIR}/**/*`)
 		.pipe(ghPages({ branch: 'master' }))
 )
 
-gulp.task('default', gulp.series('copy', 'html'))
+// build site to dest dir
+gulp.task('default', gulp.series('minify-classnames', 'copy-other-files', 'minify-html-inline', 'cleanup-build'))
+// build and deploy site
 gulp.task('deploy', gulp.series('default', 'gh-pages'))
+// dev env
 gulp.task('dev', gulp.series('default', 'serve'))
--- public/script.js
+++ public/script.js
@@ -3,7 +3,7 @@
         const wrapNameLetters = name => (name.split('').map(item => item !== ' ' ? `<span>${item}</span>` : ' ').join(''))
         const generateKerning = () => {
             const kerningSize = [ 55, 45, 45, 45, 0, 35, 4, 35, 1, 55 ];
-            const kerningTemplate = (value, i) => `.my-name-is span:nth-child(${i}){margin-right:-.${value}vmin}`;
+            const kerningTemplate = (value, i) => `.pm--my-name-is span:nth-child(${i}){margin-right:-.${value}vmin}`;
             const kerningStyle = kerningSize.reduce((value, item, i) => {
                 if (item > 0) {
                     value += kerningTemplate(item, i+1);
@@ -24,7 +24,7 @@
             document.head.appendChild(styletag);
         }
         
-        const nameContainer = document.querySelector('.my-name-is');
+        const nameContainer = document.querySelector('.pm--my-name-is');
         const name = nameContainer.innerHTML;
         const wrappedName = wrapNameLetters(name);
         const kerningStyle = generateKerning();
