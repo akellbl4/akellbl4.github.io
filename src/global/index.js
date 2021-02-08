3d30b37fb4878e590fbd0b17e882360ea0edb93c
Pavel Mineev <pavel@mineev.me>
2021-02-08 19:14:57 +0300

    clean js code

--- src/global/index.js
+++ src/global/index.js
@@ -1,42 +1,37 @@
-;((window, document) => {
-	const wrapNameLetters = (name) =>
-		name
-			.split('')
-			.map((item) => (item !== ' ' ? `<span>${item}</span>` : ' '))
-			.join('')
+const d = document
+const wrapNameLetters = (name) =>
+	name
+		.split('')
+		.map((item) => (item !== ' ' ? `<span>${item}</span>` : ' '))
+		.join('')
 
-	const generateKerning = () => {
-		const kerningSize = [55, 45, 45, 45, 0, 35, 4, 35, 1, 55]
-		const kerningTemplate = (value, i) => `.pm--my-name-is span:nth-child(${i}){margin-right:-.${value}vmin}`
-		const kerningStyle = kerningSize.reduce((value, item, i) => {
-			if (item > 0) {
-				value += kerningTemplate(item, i + 1)
-			}
+const generateKerning = () => {
+	const kerningSize = [55, 45, 45, 45, 0, 35, 4, 35, 1, 55]
+	const kerningTemplate = (value, i) => `.pm--my-name-is span:nth-child(${i}){margin-right:-.${value}vmin}`
+	const kerningStyle = kerningSize.reduce((value, item, i) => {
+		if (item > 0) {
+			value += kerningTemplate(item, i + 1)
+		}
 
-			return value
-		}, '')
+		return value
+	}, '')
 
-		return kerningStyle
-	}
+	return kerningStyle
+}
 
-	const insertStyles = (css) => {
-		const styletag = document.createElement('style')
+const insertStyles = (css) => {
+	const styleElement = d.createElement('style')
 
-		styletag.type = 'text/css'
-		styletag.appendChild(document.createTextNode(css))
+	styleElement.appendChild(d.createTextNode(css))
+	d.head.appendChild(styleElement)
+}
 
-		document.head.appendChild(styletag)
-	}
+try {
+	const nameContainer = d.querySelector('.pm--my-name-is')
+	const name = nameContainer.innerHTML
+	const wrappedName = wrapNameLetters(name)
+	const kerningStyle = generateKerning()
 
-	try {
-		const nameContainer = document.querySelector('.pm--my-name-is')
-		const name = nameContainer.innerHTML
-		const wrappedName = wrapNameLetters(name)
-		const kerningStyle = generateKerning()
-
-		nameContainer.innerHTML = wrappedName
-		insertStyles(kerningStyle)
-
-		window.__inited = 1
-	} catch (e) {}
-})(window, document)
+	nameContainer.innerHTML = wrappedName
+	insertStyles(kerningStyle)
+} catch (e) {}
